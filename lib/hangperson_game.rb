90367e696fb66c276045236efacb575c9b8ee97b
EC2 Default User <ec2-user@ip-172-31-82-95.ec2.internal>
2018-02-25 11:37:50 +0000

    Ready for Heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,6 @@
 class HangpersonGame
 
+  
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -7,21 +8,58 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  attr_accessor :word, :guesses, :wrong_guesses, :valid
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @valid
   end
 
-  # You can test it by running $ bundle exec irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
-  #  => "cooking"   <-- some random word
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.new('watchout4snakes.com').start { |http|
-      return http.post(uri, "").body
-    }
+    Net::HTTP.post_form(uri ,{}).body
   end
+  
+  def guess (letter)
+    # raise Argumenterror
+    raise ArgumentError if letter.nil? || letter.empty? || letter=~/[^A-Za-z]/
+   
+    letter = letter.downcase
+    if (@guesses.include?(letter)) || (@wrong_guesses.include?(letter)) #Check if guesses or wrong-guesses includes the given letter
+      false                         #return false if it does
+    else                            #If it doesn't include the given letter execute the following block
+      if @word.include? letter      #If letter matches any single string in word
+        @guesses << letter        #add letter to guesses
+      else                          #If it doesn't include then we put it in wrong guesses
+        @wrong_guesses << letter
+      end
+    end
+  end
+  
+  def word_with_guesses
+    
+    if guesses.empty?
+      "-"*@word.size
+    else
+      @word.gsub(/[^#{@guesses}]/, "-")
+    end
+  end
+    
+ def check_win_or_lose
+   if @wrong_guesses.length >= 7
+     @valid = :lose
+     :lose
+    elsif @word == self.word_with_guesses
+      @valid = :win
+      :win
+    else
+      @valid = :play
+      :play
+   end
+ end
+
 
 end
